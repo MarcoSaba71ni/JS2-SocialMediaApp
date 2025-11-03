e7a712c3f77893f2b52c637ea0a673dc26c0297c
Marco Sabatini <marsab04188@stud.noroff.no>
2025-11-03 09:55:44 +0100

    Feed styling and converting to scss. Pagination and Sticky footer

--- js/src/feed.js
+++ js/src/feed.js
@@ -1,43 +1,55 @@
-/*import { checkAuth } from "../utils/checkAuth.js";
+import { checkAuth } from "../utils/checkAuth.js";
 import { getAllPosts } from "../api/posts.js";
 import { getToken } from "../storage/local.js";
-import {postContent} from "../components/renderPosts.js";
+import { postContent } from "../components/renderPosts.js";
 import { searchSetup } from "../components/searchBar.js";
-
 
 // checkAuth();
 
-function renderPost(postsArray) {
-    const feedPosts = document.getElementById('feed-posts');
-    feedPosts.innerHTML = "";
+const feedPosts = document.getElementById("feed-posts");
+const loadMoreBtn = document.getElementById("load-more-btn");
 
-      if (postsArray.length === 0) {
+let allPosts = [];
+let visibleCount = 3; 
+
+function renderPost() {
+  feedPosts.innerHTML = "";
+
+  const visiblePosts = allPosts.slice(0, visibleCount);
+
+  if (visiblePosts.length === 0) {
     feedPosts.innerHTML = "<p>No posts found.</p>";
     return;
   }
 
-    postsArray.forEach(post => {
-        const postElement = postContent(post);
-        feedPosts.appendChild(postElement);
-    })
+  visiblePosts.forEach((post) => {
+    const postElement = postContent(post);
+    feedPosts.appendChild(postElement);
+  });
+
+  if (visibleCount >= allPosts.length) {
+    loadMoreBtn.style.display = "none";
+  } else {
+    loadMoreBtn.style.display = "block";
+  }
 }
 
 async function loadFeed() {
+  try {
+    const token = getToken();
+    const posts = await getAllPosts(token);
+    allPosts = posts.data;
 
-    try {
-        const token = getToken();
-        const posts = await getAllPosts(token);
-        const allPosts = posts.data;
-
-        renderPost(allPosts);
-        searchSetup(allPosts, renderPost);
-
-
-    } catch (error) {
-        feedPost.innerHTML = "<p>Could not load posts.</p>";
-    }
+    renderPost();
+    searchSetup(allPosts, renderPost);
+  } catch (error) {
+    feedPosts.innerHTML = "<p>Could not load posts.</p>";
+  }
 }
 
+loadMoreBtn.addEventListener("click", () => {
+  visibleCount += 3; 
+  renderPost();
+});
 
-
-loadFeed(); */
+loadFeed();
